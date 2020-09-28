e37d2338bd8d59a390cdef1324217f6a2cf0570c
Rohit <rohit.ray@iimjobs.com>
2020-09-28 13:33:19 +0530

    Pizza

--- hungry.py
+++ hungry.py
@@ -1,7 +1,10 @@
 hungry=input("are you hungry?")
 if hungry=="Yes":
     print("eat samosa")
+<<<<<<< HEAD
     print("eat pizza")
     pritn("eat burger")
+=======
+>>>>>>> parent of e5214e7... Pizza
 else:
     print("do your homework")
